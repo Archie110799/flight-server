ff0487953fd5714f9147b27cb9405c5f68ca9716
admin <admin@manhph-macos2.local>
2023-04-15 15:26:54 +0700

    add drawl

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getHello() {
+    getBrowserProxy: async (proxy) => {
+      return await puppeteer.launch({
+        headless: true,
+        ignoreHTTPSErrors: true,
+        args: [
+          '--proxy-server=' + proxy,
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--start-maximized',
+        ],
+      });
+    };
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        const browser = await puppeteer.launch();
+        let dataObj = {};
+
+        const page = await browser.newPage();
+        const url =
+          'https://www.vemaybay.vn/flight-result?request=HANSGN25042023-1-0-0';
+        await page.goto(url);
+        // Wait and click on first result
+
+        const res = await page.$$eval('.ftl-flight-main', (rows) => {
+          return Array.from(rows, (row) => {
+            const columns = row.querySelectorAll('ul');
+            return Array.from(columns, (column) => column.innerText);
+          });
+        });
+
+        dataObj['flightList'] = res[0];
+
+        await browser.close();
+        return resolve(dataObj);
+      } catch (e) {
+        return reject(e);
+      }
+    });
     return 'Hello World!';
   }
 }
